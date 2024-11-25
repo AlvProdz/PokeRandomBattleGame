dcd89711b6dd12c36ac646012b3bed2ab3f5e285
Álvaro Perera <alvprodz@gmail.com>
2024-11-25 18:04:23 +0100

    20241125_APR_FirstActivity
    -First activity ready to implement.
    -SQLite dependencies installed.
    -Player class to store data in Sqlite ready.

--- app/src/main/java/com/example/pokerandombattle/activities/MainMenu.kt
+++ app/src/main/java/com/example/pokerandombattle/activities/MainMenu.kt
@@ -1,12 +1,13 @@
-package com.example.pokerandombattle
+package com.example.pokerandombattle.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.pokerandombattle.R
 
-class MainActivity : AppCompatActivity() {
+class MainMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
